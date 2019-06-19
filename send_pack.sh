3d4f1be9943cbd9da231141442a9e9741ce6f0cc
Lain Iwakura <>
2019-06-19 17:44:26 +0200

    Updated resource-pack.

--- send_pack.sh
+++ send_pack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SUM=$(sha1sum resourcepack.zip | awk '{ print $1 }')
 cp resourcepack.zip "$SUM.zip"
-echo "Set the resource pack path to: https\\://www.domain.com/$SUM.zip"
 git add .
 git commit -m "Updated resource-pack."
 git push origin master
+echo "Set the resource pack path to: https\\://github.com/tyrerexus/ResourcePack/raw/master/$SUM.zip"
